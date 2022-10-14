58984fe9c10add8afb9af7ab855ce392a8f7bb65
Simon yang <simon.yang.sh@gmail.com>
2022-10-14 12:03:16 +0800

    bug fix

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron');
+const serve = require('electron-serve');
+
+const loadURL = serve({directory: 'docs/.vitepress/dist'});
+
+let mainWindow;
+
+(async () => {
+	await app.whenReady();
+
+	mainWindow = new BrowserWindow();
+
+	await loadURL(mainWindow);
+
+	// The above is equivalent to this:
+	await mainWindow.loadURL('app://localhost');
+	// The `-` is just the required hostname
+})();
